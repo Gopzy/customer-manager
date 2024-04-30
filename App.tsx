b10d71a29de1339e351557cce9a5aa7d0be78510
Goavardanan Yogarajah <goavardanan@rhino-partners.com>
2024-05-01 00:42:10 +0530

    stack navigation added

--- App.tsx
+++ App.tsx
@@ -2,13 +2,24 @@ import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/homeScreen";
 import configureStore from "./store/configureStore";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import customerDetailsScreen from "./screens/customerDetailsScreen";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <Provider store={configureStore}>
-      <View style={styles.container}>
-        <HomeScreen />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name={"HomeScreen"} component={HomeScreen} />
+          <Stack.Screen
+            name={"customerDetailsScreen"}
+            component={customerDetailsScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
--- components/customerListItem.tsx
+++ components/customerListItem.tsx
@@ -1,29 +1,43 @@
 import React from "react";
-import { View, Text, Pressable, Image, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import {
+  View,
+  Text,
+  Pressable,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import { useDispatch } from "react-redux";
 import colors from "../constants/colors";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 const CustomerListItem = ({ item }) => {
   const dispatch = useDispatch();
+  const navigation = useNavigation();
   const { first_name, email, number, gender, photo } = item;
 
   return (
-    <View key={item.id} style={style.container}>
+    <TouchableOpacity
+      key={item.id}
+      style={style.container}
+      onPress={() =>
+        navigation.navigate("customerDetailsScreen", { customer: item })
+      }
+    >
       <View>
-        {/* <Text style={style.itemName}>{`size: ${first_name}`}</Text> */}
         <View style={style.quantity}>
           <Image source={{ uri: photo }} style={style.imgStyle} />
           <View>
             <Text style={style.quantityTxt}>{`name: ${first_name}`}</Text>
-            <Text style={style.size}>{`e-mail: ${number}`}</Text>
-            <Text
-              style={[style.size, style.font_15]}
-            >{`gender: ${gender}`}</Text>
+            <Text style={style.size}>{`number: ${number}`}</Text>
+            <Text style={[style.size, style.font_15]}>{`gender: ${
+              gender === "M" ? "Male" : "Female"
+            }`}</Text>
           </View>
         </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 const style = StyleSheet.create({
@@ -38,7 +52,6 @@ const style = StyleSheet.create({
     alignItems: "center",
     backgroundColor: colors.cartgreen,
   },
-
   quantity: {
     flexDirection: "row",
     alignItems: "center",
@@ -46,18 +59,6 @@ const style = StyleSheet.create({
   quantityTxt: {
     paddingLeft: 10,
     fontSize: 18,
-  },
-
-  deleteBtn: {
-    flexDirection: "column",
-    marginTop: 5,
-    alignItems: "flex-end",
-  },
-  itemName: {
-    fontSize: 16,
-    fontWeight: "500",
-    marginBottom: 10,
-    maxWidth: 250,
   },
   imgStyle: {
     width: 80,
@@ -69,26 +70,8 @@ const style = StyleSheet.create({
     fontSize: 18,
     marginTop: 5,
   },
-  btn: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    backgroundColor: colors.fadedGreen,
-    borderRadius: 5,
-    width: 90,
-  },
-  btnTxt: {
-    color: "white",
-    paddingHorizontal: 10,
-  },
-  font_25: {
-    fontSize: 25,
-  },
   font_15: {
     fontSize: 15,
-  },
-  font_20: {
-    fontSize: 20,
   },
 });
 
